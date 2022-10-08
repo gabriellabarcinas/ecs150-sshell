774c185a293bcb9d61626b1cb3c04b126955b9c8
Gabby Barcinas <gnbarcinas@ucdavis.edu>
2022-10-07 17:40:18 -0700

    sshell.c

--- sshell.c
+++ sshell.c
@@ -5,6 +5,38 @@
 
 #define CMDLINE_MAX 512
 
+void parsecmd(char cmd[], char* args[]) {
+        char *arg;
+        int i = 0;
+
+        arg = strtok(cmd, " "); 
+
+        while (arg != NULL){
+                printf(" %s\n", arg);
+                args[i] = arg;
+                i++;
+                arg = strtok(NULL, " ");
+        } 
+}
+
+// fork - execute - wait
+int run(char cmd[]) {
+
+//        pid_t pid;
+       char* args[CMDLINE_MAX];
+       
+       parsecmd(cmd, args); 
+       
+       if(fork() == 0) { // Child Process
+                execvp(command, );
+                perror("execv");
+                exit(1);
+       } else { // Parent Process
+                wait
+       }
+        return 0;
+}
+
 int main(void)
 {
         char cmd[CMDLINE_MAX];
@@ -38,9 +70,11 @@ int main(void)
                 }
 
                 /* Regular command */
-                retval = system(cmd);
+                retval = run(cmd);
                 fprintf(stdout, "Return status value for '%s': %d\n",
                         cmd, retval);
         }
         return EXIT_SUCCESS;
 }
+
+
